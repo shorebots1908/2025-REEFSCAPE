cd498f28b45b3c3977ba2a11ee532bb114b1eb48
Thomas <geauxpher123@gmail.com>
2025-01-12 14:57:53 -0500

    initial commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "frc2025yeah";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-01-12 14:52:32 EST";
+  public static final long BUILD_UNIX_TIME = 1736711552163L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants() {}
+}
